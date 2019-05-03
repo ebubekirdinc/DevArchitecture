0a013b53f76b869077059b8d8dbbeaa6a15a1a9b
Murat Kurtboğan <kurtbogan88@gmail.com>
2019-05-03 14:07:29 +0300

    LogAspectinin yazılması

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,7 @@
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
+using Core.Aspects.Autofac.Logging;
 using Core.BackgroundJobs;
 using Core.CrossCuttingConcerns.Caching.Microsoft;
 using Core.Utilities.Mail;
@@ -13,6 +14,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+using Core.CrossCuttingConcerns.Logging.Log4Net;
+using Core.CrossCuttingConcerns.Logging.Log4Net.Loggers;
 
 namespace Business.Concrete
 {
@@ -47,6 +50,7 @@ namespace Business.Concrete
         
 
         [ValidationInterceptionAspect(typeof(ProductValidation))]
+        [LoggingInterceptionAspect(typeof(FileLogger))]
         public void Add(Product product)
         {
             _productDal.Add(product);
---
+++ Core/Aspects/Autofac/Logging/LoggingInterceptionAspect.cs
@@ -0,0 +1,41 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Logging.Log4Net;
+using Core.Utilities.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Aspects.Autofac.Logging
+{
+
+    public class LoggingInterceptionAspect : MethodInterception
+    {
+        private LoggerService _loggerService;
+        public LoggingInterceptionAspect(Type loggerType)
+        {
+            if (loggerType.BaseType != typeof(LoggerService))
+                throw new Exception("Wrong Logger Type");
+
+            _loggerService = (LoggerService)Activator.CreateInstance(loggerType);
+        }
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var logParameters = invocation.Arguments.Select(x => new LogParameter
+            {
+                Type = x.GetType().Name,
+                Value = x
+            }).ToList();
+
+            var logDetail = new LogDetail()
+            {
+                FullName = invocation.TargetType.Name,
+                MethodName = invocation.Method.Name,
+                Parameters = logParameters
+            };
+
+            _loggerService.Error(logDetail);
+        }
+    }
+}
